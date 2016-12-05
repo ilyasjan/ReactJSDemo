fcde570672cb37110e0f7bdb50b3d7efd3c1a54a
Mr.Fish <ilyas5137@163.com>
2016-12-05 16:30:47 +0600

    Demo Project Almost finished

--- js/babola.js
+++ js/babola.js
@@ -1,7 +1,44 @@
-var menu1=document.getElementById('menu-1');
-var menu2=document.getElementById('menu-1');
-var con=document.getElementById('con');
-ReactDOM.render(
-    <h1>hhnhnhnhnt</h1>,
-    con
-);
+var menu=document.getElementById('Menu');
+
+var container=document.getElementById('ArtCon');
+
+var Text=React.createClass({
+
+    render:function(){
+        return <h1>{this.props.text}</h1>;
+
+    }
+});
+
+var ArtItem=React.createClass({
+    
+    getInitialState: function() {
+        return {item:this.props.item};
+    },
+    
+    handleClick:function(e){
+        ReactDOM.render(<Text text={this.state.item.text}/>,container);
+    },
+    
+    render:function(){
+        return <a href="#" className="list-group-item" onClick={this.handleClick} >{this.props.item.author}</a>;
+    }
+    
+});
+
+var ArtList=React.createClass({
+    render:function(){
+        var nodes=this.props.data.map(function(item){ return <ArtItem item={item}/>; });
+        return <div className="list-group">{nodes}</div>;
+    }
+});
+
+$.ajax({
+    url:'/dm.json',
+    type:'get',
+    dataType:'json',
+    success:function(d){
+        ReactDOM.render(<ArtList data={d}/>,menu);        
+    }
+});
+
